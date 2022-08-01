05c30dccad0fd3873a8084d62b442f909b84e3ce
franciscogalmes <franciscogalmes@gmail.com>
2022-08-01 00:35:15 -0300

    Agrego carpeta de JavaScript

---
+++ JS Practice/JavaScript.js
@@ -0,0 +1,11 @@
+<script>
+        
+const para = document.querySelector('p');
+para.addEventListener('click', updateName);
+
+function updateName() {
+let name = prompt('Enter a new name');
+para.textContent = 'Player 1: ' + name;};
+
+</script>
+
